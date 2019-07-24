9c2b221ac3fa8021dd4a7843d9446020b48b093e
Chris Haining <52139935+chrishaining@users.noreply.github.com>
2019-07-24 15:00:56 +0100

    Add files via upload

---
+++ quotes.js
@@ -0,0 +1,29 @@
+
+
+quotes = new Array()
+
+quotes[0] = "It ain't over till it's over";
+
+quotes[1] = "When you come to a fork in the road, take it.";
+
+quotes[2] = "You can observe a lot by just watching.";
+
+quotes[3] = "If you don't know where you are going, you'll end up someplace else.";
+
+quotes[4] = "It's like déjà vu all over again.";
+
+quotes[5] = "The future ain't what it used to be.";
+
+quotes[6] = "No one goes there nowadays, it’s too crowded.";
+
+quotes[7] = "Baseball is ninety percent mental and the other half is physical.";
+
+quotes[8] = "In theory there is no difference between theory and practice. In practice there is.";
+
+quotes[9] = "You've got to be very careful if you don't know where you are going, because you might not get there.";
+
+var i = Math.floor(Math.random() * quotes.length);
+
+
+document.write(quotes[i]);
+//document.getElementById('quoteDisplay').inner.HTML * quotes[randomNumber];
